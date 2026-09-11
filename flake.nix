1f5a9c46c20df792e8fd64d698fd243f0f88cdd6
EngeloX <evilguroff@gmail.com>
2026-09-12 02:39:12 +0600

    add nixOS flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
