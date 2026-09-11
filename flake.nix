454db26c8b92ecfd7ddba98aa19206c8b3eab6f4
EngeloX <evilguroff@gmail.com>
2026-09-12 02:49:11 +0600

    add Home manager and Plasma manager

--- flake.nix
+++ flake.nix
@@ -3,14 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { nixpkgs, ... }: {
+  outputs = { nixpkgs, home-manager, plasma-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       modules = [
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+
+        {
+          home-manager.sharedModules = [
+            plasma-manager.homeModules.plasma-manager
+          ];
+
+          home-manager.users.rassik = import ./home/rassik.nix;
+        }
       ];
     };
   };
---
+++ home/rassik.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+
+{
+  home.username = "rassik";
+  home.homeDirectory = "/home/rassik";
+
+  home.stateVersion = "26.05";
+}
