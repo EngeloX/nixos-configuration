bd18567bd095ab93811f0357f6c09643ffe37e0a
EngeloX <evilguroff@gmail.com>
2026-09-13 10:24:26 +0600

    kde customization

--- apps.nix
+++ apps.nix
@@ -4,8 +4,16 @@
 {
   # user apps
   environment.systemPackages = with pkgs; [
+      # apps
       telegram-desktop
       discord
+
+      # Widgets
+      plasma-panel-colorizer
+      kdePackages.spectacle
+      python3
+      python3Packages.dbus-python
+      python3Packages.pygobject3
     ];
 
   programs.steam.enable = true;
--- home/kde.nix
+++ home/kde.nix
@@ -6,20 +6,15 @@
 
   home.stateVersion = "26.05";
 
-  # wallpaper
-  #stylix = {
-  #  enable = true;
-  #  overrideConfig = true;
-  #  image = ./path/to/your/wallpaper.jpg; # Path to your image file
-  #};
-
   # task panel on all displays
   programs.plasma = {
     enable = true;
 
     # Overwrites manual GUI changes with this config
     # overrideConfig = true;
-
+    workspace = {
+      wallpaper = ./images/yuno.jpg;
+    }
     panels = [
       {
         location = "bottom";
