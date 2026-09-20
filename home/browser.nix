57e8f1959ffbc731cf889ef15287d1825d2d1269
EngeloX <evilguroff@gmail.com>
2026-09-20 19:22:47 +0600

    mini fix firefox

--- home/browser.nix
+++ home/browser.nix
@@ -3,13 +3,31 @@
 
 {
   programs.firefox = {
-   enable = true;
+    enable = true;
 
-   profiles.default = {
-     settings = {
-       "browser.startup.page" = 0;
-       "browser.urlbar.suggest.calculator" = true;
-     };
-   };
+    profiles.default = {
+      settings = {
+        "browser.startup.page" = 0;
+        "browser.urlbar.suggest.calculator" = true;
+      };
+    };
+  };
+
+  # Firefox as the default browser
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+    };
+
+    associations.added = {
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+    };
   };
 }
+
